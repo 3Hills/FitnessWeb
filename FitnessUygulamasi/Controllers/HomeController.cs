30ce9254d7156deab749b295931f9433a756c647
Alican Üçtepe <alicanuc@gmail.com>
2017-09-08 14:24:02 +0300

    Veritabanı işlemlerinden önceki son düzenlemeler yapıldı.

--- FitnessUygulamasi/Controllers/HomeController.cs
+++ FitnessUygulamasi/Controllers/HomeController.cs
@@ -8,8 +8,29 @@ namespace FitnessUygulamasi.Controllers
 {
     public class HomeController : Controller
     {
+        // Dashboard sayfası.
         public ActionResult Index()
         {
+            return View();
+        }
+
+        // Yeni hareket ekleme sayfası.
+        public ActionResult YeniHareketEkle() {
+            return View();
+        }
+
+        // Yeni antrenman ekleme sayfası.
+        public ActionResult YeniAntrenmanEkle() {
+            return View();
+        }
+
+        // Antrenmana hareket ekleme sayfası.
+        public ActionResult AntrenmanaHareketEkle() {
+            return View();
+        }
+
+        // Harekete set ekleme sayfası.
+        public ActionResult HareketeSetEkle() {
             return View();
         }
     }
